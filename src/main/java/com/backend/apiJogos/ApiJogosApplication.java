290fbcacca415d857c7f960c0a06288f4920e8ce
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2026-04-20 10:55:29 -0300

    feat: inicialização

---
+++ src/main/java/com/backend/apiJogos/ApiJogosApplication.java
@@ -0,0 +1,13 @@
+package com.backend.apiJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/apiJogos/ApiJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.apiJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
