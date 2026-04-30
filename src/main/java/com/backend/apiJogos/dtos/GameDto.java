f184d8817ffaeb83ade98754ca5df1733be117e6
sfrancileidy-prog <sfrancileidy@gmail.com>
2026-04-30 17:50:18 -0300

    feat: adiciona CRUD de games

---
+++ src/main/java/com/backend/apiJogos/dtos/GameDto.java
@@ -0,0 +1,26 @@
+package com.backend.apiJogos.dtos;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameDto {
+     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+     private UUID id;
+
+     @NotNull(message = "O nome não pode ser nulo")
+     private String nome;
+
+}
